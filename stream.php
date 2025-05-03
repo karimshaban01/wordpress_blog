28db92d64adb714dfc5814eb7e13c3ec31eaefcd
karim haruna <karimxhaban@gmail.com>
2025-05-03 07:29:54 +0300

    v1

---
+++ stream.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>VLC Stream</title>
+</head>
+<body>
+    <h1>Live Stream</h1>
+    <video controls autoplay>
+        <source src="http://192.168.0.52:8080/" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+</body>
+</html>
